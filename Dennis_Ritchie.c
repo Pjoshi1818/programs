1f873f879a6a4b05f92ccadff100365df5b17708
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-06-26 21:16:42 +0530

    fist program

---
+++ Dennis_Ritchie.c
@@ -0,0 +1,12 @@
+                        //chapter 1 //
+                         //basic to advanced concepts of C programming language//
+
+
+#include <stdio.h>
+int main(){
+
+    printf("hello, world");
+
+    return 0;
+}
+
