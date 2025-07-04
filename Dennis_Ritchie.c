01e628bb99d91ba3f7c22b6591310e09459dfd7b
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-07-04 22:50:43 +0530

    C

--- Dennis_Ritchie.c
+++ Dennis_Ritchie.c
@@ -271,8 +271,76 @@
 // }
 
 
-#include<stdio.h>
-int main(){
-    
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#define MAXLINE 1000
+
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+
+int my_getline(void);
+void copy(void);
+
+int main()
+{
+    int len;
+
+    max = 0;
+    while ((len = my_getline()) > 0) {
+        if (len > max) {
+            max = len;
+            copy();
+        }
+    }
+
+    if (max > 0) {
+        printf("Longest line:\n%s", longest);
+    }
+
     return 0;
+}
+
+int my_getline(void)
+{
+    int c, i;
+    for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        line[i] = c;
+    }
+
+    if (c == '\n') {
+        line[i] = c;
+        ++i;
+    }
+
+    line[i] = '\0';
+    return i;
+}
+
+void copy(void)
+{
+    int i = 0;
+    while ((longest[i] = line[i]) != '\0') {
+        ++i;
+    }
+}
+
+
+
+
+
+void copypj(char to[],char from[]){
+    int i ;
+    i = 0;
+    while((to[i]= from[i]) != '\0'){
+        ++i;
+    }
+    
 }
